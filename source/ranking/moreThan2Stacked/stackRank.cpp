ae16511849a8ce7d1a627671f6ee37c6f6e098ce
Grant Farnsworth <grantfar@gmail.com>
2018-12-09 13:37:00 -0500

    added ranking System

---
+++ source/ranking/moreThan2Stacked/stackRank.cpp
@@ -0,0 +1,5 @@
+#include "stackRank.hpp"
+int stackRank(schedule* s)
+{
+    return 0;
+}
---
+++ source/ranking/rankfunctions.cpp
@@ -0,0 +1,12 @@
+#include "rank.hpp"
+#include "moreThan2Stacked/stackRank.hpp"
+
+
+void rankFunctions()
+{
+    rank::Functions.push_back(
+        rankFunction{
+            "To Many Classes In one Day",*stackRank
+        }
+    );
+}
