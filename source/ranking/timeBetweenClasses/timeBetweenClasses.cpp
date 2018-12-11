afd4cf95e880330e6577ff34ba173f2d05cf8b82
Grant Farnsworth <grantfar@gmail.com>
2018-12-11 18:52:56 -0500

    fixed courseSection And timeBetweenClasses

--- source/ranking/timeBetweenClasses/timeBetweenClasses.cpp
+++ source/ranking/timeBetweenClasses/timeBetweenClasses.cpp
@@ -1,14 +1,36 @@
 #include "timeBetweenClasses.hpp"
+#include <vector>
 
+using namespace std;
+
+void splitIntoDays(schedule * s, vector<courseSection*>* days)
+{
+    for(int i = 0; i< s->getCourseCount(); i++)
+    {
+        for(int j = 0; j < 5; j++)
+        {
+            if(s->getEnrollments()[i]->OnDay(j))
+            {
+                days[j].push_back(s->getEnrollments()[i]);
+            }
+        }
+    }
+}
 
 int timeBetweenClasses(schedule * s)
 {
     int gapCount = 0;
     s->sortSchedule();
-    for(int i = 0; i < s->getCourseCount() -1; i++){
-        for(int j = 0; j < s->getCourseCount(); j++)
+    vector<courseSection*> daySplit[5];
+    splitIntoDays(s,daySplit);
+
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < daySplit[i].size() - 1; j++)
         {
-            
+            if(daySplit[i][j+1]->getStartTime() - daySplit[i][j]->getEndTime() >= 130)
+                gapCount++;
         }
     }
+    return gapCount;
 }
