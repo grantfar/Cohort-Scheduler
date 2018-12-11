eef0446ff960bd9fe3991cfae501d2a01758f0d9
Alex Markules <alex.r.markules@wmich.edu>
2018-12-11 10:33:11 -0500

    fixed some testing in schedule

--- source/webApp/client/app/scheduling/scheduling.component.spec.ts
+++ source/webApp/client/app/scheduling/scheduling.component.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
-import { CohortsComponent } from './scheduling.component';
+import { SchedulingComponent } from './scheduling.component';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {HttpParams} from '@angular/common/http';
+import { SchedulingService } from '../services/scheduling.service';
+import { ToastComponent } from '../shared/toast/toast.component';
 
-/*describe('Component: Cohorts', () => {
+describe('Component: Scheduling', () => {
   it('should create an instance', () => {
-    let component = new CohortsComponent();
+  	let serv: SchedulingService;
+  	let form: FormBuilder;
+  	let toast: ToastComponent
+    let component = new SchedulingComponent(serv, form, toast);
     expect(component).toBeTruthy();
   });
-});*/
+});
