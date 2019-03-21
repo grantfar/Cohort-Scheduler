6b51afd328c9f6eabf256df9984418cf72f51997
Alex Markules <amarlar2354@gmail.com>
2019-03-21 09:44:03 -0400

    cleaning

--- source/CohortsScheduling/src/test/java/CohortScoring/HardScoringTest.java
+++ source/CohortsScheduling/src/test/java/CohortScoring/HardScoringTest.java
@@ -1,10 +1,12 @@
 package CohortScoring;
-import java.junit.*;
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.*;
 import CohortDataClasses.Cohort;
 import CohortDataClasses.CohortSectionAssignment;
 import CohortDataClasses.Course;
@@ -14,27 +16,60 @@ import CohortDataClasses.CohortSectionAssignment;
 
 public class HardScoringTest {
 
-	HardScoringFunctions hsf = null;
-
-	@Before
-	public void init(){
-		hsf = new HardScoringFunctions();
-	}
-
 	@Test
 	public void calculateOverEnrolled_Test1(){
 		List<Course> arg = new ArrayList<>();
 		Course c = new Course();
 		Section s = new Section();
-		s.setSeats(10)
+		s.setSeats(10);
 		s.setEnrolled(11);
 		List<Section> sects = new ArrayList<>();
 		sects.add(s);
 		Section s1 = new Section();
-		s1.setSeats(10)
+		s1.setSeats(10);
 		s1.setEnrolled(10);
 		sects.add(s1);
-		arg.setSections(sects);
-
+		c.setSections(sects);
+		arg.add(c);
+		int retVal = HardScoringFunctions.calculateOverEnrolledScore(arg);
+		assertTrue(retVal==-1);
+	}
+	
+	@Test
+	public void calculateOverEnrolled_Test2(){
+		List<Course> arg = new ArrayList<>();
+		Course c = new Course();
+		Section s = new Section();
+		s.setSeats(10);
+		s.setEnrolled(15);
+		List<Section> sects = new ArrayList<>();
+		sects.add(s);
+		Section s1 = new Section();
+		s1.setSeats(10);
+		s1.setEnrolled(20);
+		sects.add(s1);
+		c.setSections(sects);
+		arg.add(c);
+		int retVal = HardScoringFunctions.calculateOverEnrolledScore(arg);
+		assertTrue(retVal==-3);
+	}
+	
+	@Test
+	public void calculateOverEnrolled_Test3(){
+		List<Course> arg = new ArrayList<>();
+		Course c = new Course();
+		Section s = new Section();
+		s.setSeats(10);
+		s.setEnrolled(0);
+		List<Section> sects = new ArrayList<>();
+		sects.add(s);
+		Section s1 = new Section();
+		s1.setSeats(10);
+		s1.setEnrolled(9);
+		sects.add(s1);
+		c.setSections(sects);
+		arg.add(c);
+		int retVal = HardScoringFunctions.calculateOverEnrolledScore(arg);
+		assertTrue(retVal==0);
 	}
 }
